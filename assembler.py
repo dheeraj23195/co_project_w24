124089ff4d5de001bb1e5450854dd2cf31759166
iamgarvit <garvit4356@gmail.com>
2024-03-06 00:14:28 +0530

    updated assembler.py (testing connectivity)

--- assembler.py
+++ assembler.py
@@ -1,3 +1,13 @@
 #turns assembly code into machine code
 input_lines=[]
 output_data=[]
+op_codes = {
+    "add": "0110011",
+    "jalr": "1100111",
+    "lw": "0000011",
+    "sw": "0100011",
+    "blt": "1100011",
+    "auipc": "0010111",
+    "jal": "0010111",
+    "addi": "0010011",
+}
