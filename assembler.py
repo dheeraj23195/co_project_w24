f627df447cc670b804abd8b272eb284331cec297
adit23036 <162185449+adit23036@users.noreply.github.com>
2024-03-06 18:54:50 +0530

    Update assembler.py

--- assembler.py
+++ assembler.py
@@ -6,24 +6,23 @@ output_data=[]
 
 #updating the provided information in useable format
 op_codes = { #add the opcodes
-    "add": "0110011","jalr": "1100111","lw": "0000011","sw": "0100011","blt": "1100011","auipc": "0010111",
-    "jal": "0010111","addi": "0010011",
+    "r_type_instructions": "0110011", "lw": "0000011", "sltiu": "0010011", "jalr": "1100111","sw": "0100011","blt": "1100011","auipc": "0010111",
+    "jal": "0010111",
 }
 
 r_type_instruction=[#add the instructions 
-    "add",
-                    "sub"]
+"add", "slt", "sltu", "xor", "sll", "srl", "or", "and", "sub"]
 
 i_type_instruction=[#complete the instruction list
-                    "lw","addi"
-]
+"lw", "addi", "sltiu", "jalr"]
 
-s_type_instruction=[]
+s_type_instruction=["sw"]
 
-b_type_instruction=[]
+b_type_instruction=["beq", "bne", "blt", "bge", "bltu", "bgeu"]
 
-u_type_instruction=[]
+u_type_instruction=["lui", "auipc"]
 
-j_type_instruction=[]
+j_type_instruction=["jal"]
 
-bonus_instruction=[]
+bonus_instruction=["mul", "rst", "halt", "rvrs"]
+
